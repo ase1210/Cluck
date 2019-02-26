630e84ff3d6112051163204c3825d33197b53c31
DrewE <andrew.engelstein@gmail.com>
2019-02-26 15:04:59 -0800

    Add User model validations

--- CluckApp/app/models/user.rb
+++ CluckApp/app/models/user.rb
@@ -11,4 +11,18 @@
 #
 
 class User < ApplicationRecord
+  attr_reader :password
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  def password=(pw)
+  end
+
+  def is_password?(pw)
+  end
+
+  def ensure_session_token
+  end
 end
