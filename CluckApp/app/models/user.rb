19f41adc3aab87491621c31c50a31c7666a9dc2c
DrewE <andrew.engelstein@gmail.com>
2019-02-26 15:13:43 -0800

    Add user model methods for dealing with passwords and session token

--- CluckApp/app/models/user.rb
+++ CluckApp/app/models/user.rb
@@ -12,17 +12,38 @@
 
 class User < ApplicationRecord
   attr_reader :password
+
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
+
   after_initialize :ensure_session_token
 
+  def find_by_credentials(un, pw)
+    user = User.find_by(username: un)
+    (user && user.is_password?(pw)) ? user : nil
+  end
+
   def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
   end
 
   def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
   end
 
   def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
   end
 end
