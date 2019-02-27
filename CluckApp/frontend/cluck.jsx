aed584f5d6bb0b386649ebf702d007fc61b41b1d
DrewE <andrew.engelstein@gmail.com>
2019-02-26 18:08:54 -0800

    Create store.js with configureStore, Create thunk middleware, Add store to entry file

--- CluckApp/frontend/cluck.jsx
+++ CluckApp/frontend/cluck.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-
+  const store = configureStore();
   ReactDOM.render(<h1>YAY</h1>, root)
 })
