e59fc9c401cc79bf4e3532aa6aba6e84ada870d2
DrewE <andrew.engelstein@gmail.com>
2019-02-27 15:02:32 -0800

    Update Navbar getstarted link to be a button

--- CluckApp/frontend/components/navbar/navbar.jsx
+++ CluckApp/frontend/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 const SignedIn = ({ logout, currentUser }) => (
   <>
@@ -8,31 +8,45 @@ const SignedIn = ({ logout, currentUser }) => (
   </>
 )
 
-const SignedOut = (props) => (
-  <>
-    <section className='nav-left-align'>
-      <div>
-        <Link to='/' ><img className='logo' src='/images/cluck-logo.png' alt="cluck-logo" /></Link>
-      </div>
-      <div>Why Cluck?</div>
-      <div>Solutions</div>
-      <div>Resources</div>
-      <div>Pricing</div>
-    </section>
-    <section className='nav-right-align'>
-      <div><Link to='/login'>Sign in</Link></div>
-      <div><Link to='/signup'>Get Started</Link></div>
-    </section>
-  </>
-)
+class SignedOut extends React.Component {
+  constructor(props) {
+    super(props);
+    this.routeChange = this.routeChange.bind(this);
+  }
+
+  routeChange() {
+    let path = '/signup';
+    this.props.history.push(path);
+  }
+
+  render() {
+    return (
+      <>
+        <section className='nav-left-align'>
+          <div>
+            <Link to='/' ><img className='logo' src='/images/cluck-logo.png' alt="cluck-logo" /></Link>
+          </div>
+          <div>Why Cluck?</div>
+          <div>Solutions</div>
+          <div>Resources</div>
+          <div>Pricing</div>
+        </section>
+        <section className='nav-right-align'>
+          <div><Link to='/login'>Sign in</Link></div>
+          <button className="get-started" onClick={this.routeChange}>Get Started</button>
+        </section>
+      </>
+    )
+  }
+}
 
 const NavBar = (props) => (
   <nav className='navbar'>
     {props.currentUser ?
       <SignedIn currentUser={props.currentUser} logout={props.logout} /> :
-      <SignedOut />
+      <SignedOut history={props.history} />
     }
   </nav>
 )
 
-export default NavBar;
+export default withRouter(NavBar);
