86741a351866133b2f88abb8fd2b82e4c3d1a197
DrewE <andrew.engelstein@gmail.com>
2019-02-27 11:39:54 -0800

    Add AuthRoute and ProtectedRoute util

---
+++ CluckApp/frontend/util/route_util.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Redirect } from 'react-router-dom';
+
+const mSTP = (state) => ({
+  loggedIn: Boolean(state.session.currentUser)
+});
+
+const Auth = ({ loggedIn, path, component: Component }) => (
+  <Route path={path}
+    render={(props) => (
+      loggedIn ? <Redirect to='/' /> : <Component {...props} />
+    )} />
+)
+
+export const AuthRoute = withRouter(connect(mSTP)(Auth))
+
+const Protected = ({ loggedIn, path, component: Component }) => (
+  <Route path={path}
+    render={(props) => (
+      loggedIn ? <Component {...props} /> : <Redirect to='/login' />
+    )} />
+)
+
+export const ProtectedRoute = withRouter(connect(mSTP)(Protected))
