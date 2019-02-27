03f001c90eb2f86603ec21564f3bef70a55696c0
DrewE <andrew.engelstein@gmail.com>
2019-02-27 09:53:21 -0800

    Add errros to sessionForm through login and signup containers

--- CluckApp/frontend/components/session/login_form_container.jsx
+++ CluckApp/frontend/components/session/login_form_container.jsx
@@ -3,6 +3,7 @@ import SessionForm from './session_form';
 import { login } from '../../actions/session_actions';
 
 const mSTP = (state) => ({
+  sessionErrors: state.errors.sessionErrors,
   formType: "Login"
 });
 
--- CluckApp/frontend/components/session/signup_form_container.jsx
+++ CluckApp/frontend/components/session/signup_form_container.jsx
@@ -3,6 +3,7 @@ import SessionForm from './session_form';
 import { signup } from '../../actions/session_actions';
 
 const mSTP = (state) => ({
+  sessionErrors: state.errors.sessionErrors,
   formType: "Signup"
 });
 
