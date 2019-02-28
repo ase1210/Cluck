1b90bc38c9ce6cabca3a166cb44cc1c4a2647f60
DrewE <andrew.engelstein@gmail.com>
2019-02-28 09:26:31 -0800

    Add User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users Seeds
+
+User.delete_all
+
+users = [
+  {username: "Drew", password: "tester"},
+  {username: "Mike", password: "tester"},
+  {username: "Sr. Clucks'alot", password: "tester"},
+]
+
+users.each { |user| User.create(user) }
