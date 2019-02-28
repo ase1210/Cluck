34d8809243a6ad2ac89aaeb069a6e230fa5ebfea
DrewE <andrew.engelstein@gmail.com>
2019-02-27 19:15:21 -0800

    Update session form stylesheet to style error rendering

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -5,13 +5,10 @@ class SessionForm extends React.Component {
     super(props);
     this.state = {
       form: { username: "", password: "" },
-      // remember: "unchecked",
       demo: { username: 'Drew', password: 'tester' }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDemoSubmit = this.handleDemoSubmit.bind(this);
-    // this.handleInput = this.handleInput.bind(this);
-    // this.handleRemember = this.handleRemember.bind(this);
   }
 
   componentDidMount() {
@@ -36,21 +33,16 @@ class SessionForm extends React.Component {
     this.props.demoAction(this.state.demo);
   }
 
-  // handleRemember(e) {
-  //   const newVal = this.state.remember === "checked" ? "unchecked" : "checked";
-  //   this.setState({ remember: newVal });
-  // }
-
   render() {
     const errors = this.props.sessionErrors.map(
-      (error, idx) => <li key={idx}>{error}</li>
+      (error, idx) => <li className="session-errors-item" key={idx}>{error}</li>
     )
     const preposition = this.props.formType === "Signup" ? "for" : "to";
     return (
       <div className="session-form-container">
         <h2>{this.props.formType} {preposition} Cluck</h2>
         <p>Enter your username and password</p>
-        {errors.length === 0 ? <></> : <ul>{errors}</ul>}
+        {errors.length === 0 ? <></> : <ul className='session-errors-list'>{errors}</ul>}
         <form onClick={this.props.clearErrors}>
           <input type='text' value={this.state.username}
             autoFocus="autofocus"
@@ -64,11 +56,6 @@ class SessionForm extends React.Component {
           <button onClick={this.handleSubmit}>{this.props.formType}</button>
           <br />
           <button onClick={this.handleDemoSubmit}>Demo Login</button>
-          {/* <br /> */}
-          {/* <label>
-            <input type="checkbox" value={this.state.remember}
-              onClick={this.handleRemember} />Remember me
-          </label> */}
         </form>
       </div>
     )
