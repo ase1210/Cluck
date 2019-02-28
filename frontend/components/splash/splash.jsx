85303072143eea08c92cab397510f9f796e81e7e
DrewE <andrew.engelstein@gmail.com>
2019-02-28 00:13:27 -0800

    Add styling to Splash component

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const Splash = (props) => (
-  <main>
-    <section>
-      <h1>TITLE SECTION</h1>
-    </section>
-    <section>
-      <img src="" alt='splash-image' />
-      <aside>
-        <p>DESCRIPTION TEXT AREA</p>
-        <div>
-          <button>GET STARTED BUTTON</button>
-          <div>
-            <p>Already using Cluck?</p>
-            <Link to='/login'>Sign in</Link>
-          </div>
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.routeChange = this.routeChange.bind(this);
+  }
+
+  routeChange() {
+    let path = '/signup';
+    this.props.history.push(path);
+  }
+
+  render() {
+    return (
+      <div className='parent'>
+        <div className='hero-container'>
+          <main className='main-splash'>
+            <h1 className='title'>Imagine what you'll accomplish together</h1>
+            <section className='hero1'>
+              <img src="/images/cluck-hero-img.jpg" alt='splash-image' />
+              <figcaption>Molly Moon creates a recipe for success with Cluck</figcaption>
+            </section>
+            <p>Cluck is a collaboration hub for friends, no matter what friends you have. It's a place where conversations happen, decisions are made, and information is always at your fingertips. With Cluck, your friends are better connected.</p>
+            <div>
+              <button className="get-started" onClick={this.routeChange}>GET STARTED</button>
+              <div>
+                <section>Already using Cluck? <Link to='/login'>Sign in</Link></section>
+              </div>
+            </div>
+            <img className='hero2' src="/images/cluck-hero-2.jpg" alt='splash-image' />
+            <img className='hero3' src="/images/cluck-hero-3.jpg" alt='splash-image' />
+          </main>
         </div>
-      </aside>
-    </section>
-  </main>
-)
+      </div>
+    )
+  }
+}
 
-export default Splash;
+export default withRouter(Splash);
