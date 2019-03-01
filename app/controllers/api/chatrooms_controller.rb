a82083927fc35b18122df651504b04f81d574bdb
DrewE <andrew.engelstein@gmail.com>
2019-03-01 09:35:20 -0800

    Add ChatroomsController actions #index, #show, #create, #chatroom_params

--- app/controllers/api/chatrooms_controller.rb
+++ app/controllers/api/chatrooms_controller.rb
@@ -1,2 +1,26 @@
 class Api::ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.all
+  end
+
+  def show
+    @chatroom = Chatroom.find_by(id: params[:id])
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.admin_id = current_user.id unless @chatroom.admin_id
+
+    if @chatroom.save
+      render :show
+    else
+      render json: @chatroom.errors.full_messages
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name, :channel, :admin_id)
+  end
 end
