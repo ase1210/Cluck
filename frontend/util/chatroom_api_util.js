fb34c5854c66c288760f9b18783cd232a5a994ab
DrewE <andrew.engelstein@gmail.com>
2019-03-01 09:47:29 -0800

    Add Chatroom API Utils

---
+++ frontend/util/chatroom_api_util.js
@@ -0,0 +1,26 @@
+export const getChatrooms = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/chatrooms',
+    dataType: 'json',
+  })
+);
+
+export const getChatroom = (id) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/chatrooms/${id}`,
+    dataType: 'json',
+  })
+);
+
+export const postChatroom = (chatroom) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/chatrooms`,
+    dataType: 'json',
+    data: {
+      chatroom
+    }
+  })
+);
