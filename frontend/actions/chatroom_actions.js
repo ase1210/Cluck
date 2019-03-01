bc6786fbadd26f252339efe10262571bc3156c34
DrewE <andrew.engelstein@gmail.com>
2019-03-01 10:06:18 -0800

    Add Chatroom actions and thunk actions

---
+++ frontend/actions/chatroom_actions.js
@@ -0,0 +1,32 @@
+import * as ChatroomAPIUtil from '../util/chatroom_api_util';
+
+export const RECEIVE_CHATROOMS = "RECEIVE_CHATROOMS";
+export const RECEIVE_CHATROOM = "RECEIVE_CHATROOM";
+
+const receiveChatrooms = (chatrooms) => ({
+  type: RECEIVE_CHATROOMS,
+  chatrooms
+});
+
+const receiveChatroom = (chatroom) => ({
+  type: RECEIVE_CHATROOM,
+  chatroom
+});
+
+export const fetchChatrooms = () => dispatch => (
+  ChatroomAPIUtil.getChatrooms().then(
+    chatrooms => dispatch(receiveChatrooms(chatrooms))
+  )
+);
+
+export const fetchChatroom = (id) => dispatch => (
+  ChatroomAPIUtil.getChatroom(id).then(
+    chatroom => dispatch(receiveChatroom(chatroom))
+  )
+);
+
+export const createChatroom = (chatroom) => dispatch => (
+  ChatroomAPIUtil.postChatroom(chatroom).then(
+    chatroom => dispatch(receiveChatroom(chatroom))
+  )
+);
