ea0ddd7c68dde6e5b7daca1fe3b5182d025b0d45
DrewE <andrew.engelstein@gmail.com>
2019-03-01 19:44:30 -0800

    Refactor JoinChannelFormContainer to remove mSTP

--- frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form_container.jsx
+++ frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form_container.jsx
@@ -1,16 +1,12 @@
 import { connect } from 'react-redux';
 import JoinChannelForm from './join_channel_form';
-import { selectChannels } from '../../../../../selectors/selectors';
 import { fetchChatrooms } from '../../../../../actions/chatroom_actions';
 
-const mSTP = (state) => ({
-  channels: selectChannels(state)
-});
 
 const mDTP = (dispatch) => ({
   fetchChatrooms: () => dispatch(fetchChatrooms())
 });
 
-const JoinChannelFormContainer = connect(mSTP, mDTP)(JoinChannelForm);
+const JoinChannelFormContainer = connect(undefined, mDTP)(JoinChannelForm);
 
 export default JoinChannelFormContainer;
