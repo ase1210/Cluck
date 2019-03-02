0ce41252edf629f4530df5e61e0693496443f66c
DrewE <andrew.engelstein@gmail.com>
2019-03-01 21:08:10 -0800

    Update selectChannels selector to split out subscribed and available channels

--- frontend/components/logged_in/logged_in_app.jsx
+++ frontend/components/logged_in/logged_in_app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import ChannelsIndexContainer from '../chatrooms/channels/channels_index_container';
 import NavBarContainer from '../navbar/navbar_container';
 import JoinChannelFormContainer from './chatrooms/channels/join_channels/join_channel_form_container';
+import ChannelsIndex from './chatrooms/channels/channels_index/channels_index';
 const LoggedInApp = (props) => (
   <div>
     <NavBarContainer />
     <Switch>
-      <Route exact path='/messages/channel/new' component={ChannelsIndexContainer} />
+      <Route exact path='/messages/channel/new' component={ChannelsIndex} />
       <Route exact path='/messages/channel/join' component={JoinChannelFormContainer} />
       <Redirect to='/messages' />
     </Switch>
--- frontend/selectors/selectors.js
+++ frontend/selectors/selectors.js
@@ -1,3 +1,21 @@
-export const selectChannels = (state) => (
-  Object.values(state.entities.chatrooms).filter(chatroom => chatroom.channel)
-);
+export const selectChannels = (state) => {
+  const currentUser = state.session.currentUser;
+  let channels = Object.values(state.entities.chatrooms)
+    .filter(chatroom => chatroom.channel);
+  let userChatroomIds = Object.values(state.entities.chatroomUsers)
+    .filter(chatroomUser => chatroomUser.userId === currentUser)
+    .map(chatroomUser => chatroomUser.chatroomId);
+  let subscribed = [];
+  let available = [];
+  channels.forEach(channel => {
+    if (userChatroomIds.includes(channel.id)) {
+      subscribed.push(channel);
+    } else {
+      available.push(channel);
+    }
+  });
+  return {
+    subscribed,
+    available
+  };
+};
