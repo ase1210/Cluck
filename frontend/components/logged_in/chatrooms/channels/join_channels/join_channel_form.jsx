6a5cf8db4f37c100f8764f64294b730bc5c602eb
DrewE <andrew.engelstein@gmail.com>
2019-03-01 19:43:28 -0800

    Update JoinChannelForm component to add AJAX req to fetchChatrooms when component mounts

--- frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
+++ frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ChannelsIndex from '../channels_index/channels_index';
 
-const JoinChannelForm = (props) => (
-  <div>
-    <h2>Browse Channels</h2>
-    <input type="text" placeholder='Search channels' value="" />
-    <ChannelsIndex />
-  </div>
-)
+class JoinChannelForm extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchChatrooms();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Browse Channels</h2>
+        <input type="text" placeholder='Search channels' value="" />
+        <ChannelsIndex />
+      </div>
+    )
+  }
+}
 
 export default JoinChannelForm;
