d5c568d7a9105b3915c4a1ef896d594bcfd33b2f
DrewE <andrew.engelstein@gmail.com>
2019-03-03 20:43:32 -0800

    Update LoggedInApp component to reference Add Channel form container

--- frontend/components/logged_in/logged_in_app.jsx
+++ frontend/components/logged_in/logged_in_app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import JoinChannelFormContainer from './chatrooms/channels/join_channels/join_channel_form_container';
-import ChannelsIndex from './chatrooms/channels/channels_index/channels_index';
 import SidebarContainer from './chatrooms/chatrooms_sidebar/sidebar_contrainer';
+import AddChannelFormContainer from './chatrooms/channels/add_channel/add_channel_form_container';
 
 const LoggedInApp = (props) => (
   <div>
     <Switch>
       <Route exact path='/messages/:chatroomId' component={SidebarContainer} />
-      <Route exact path='/messages/:chatroomId/new' component={ChannelsIndex} />
+      <Route exact path='/messages/:chatroomId/new' component={AddChannelFormContainer} />
       <Route exact path='/messages/:chatroomId/join' component={JoinChannelFormContainer} />
       <Redirect to='/messages/1' />
     </Switch>
