4ff7e57cc0b577c05fa0d8cffbb3e7f6facbf90e
DrewE <andrew.engelstein@gmail.com>
2019-03-03 18:02:41 -0800

    Create chatroom_user_actions and update reducer to process accordingly

---
+++ frontend/actions/chatroom_user_actions.js
@@ -0,0 +1,15 @@
+import * as ChatroomUserAPIUtil from '../util/chatroom_user_api_util';
+
+export const RECEIVE_CHATROOM_USER = "RECEIVE_CHATROOM_USER";
+
+const receiveChatroomUser = (chatroomUser) => ({
+  type: RECEIVE_CHATROOM_USER,
+  chatroomUser
+});
+
+export const createChatroomUser = (chatroomUser) => dispatch => (
+  ChatroomUserAPIUtil.postChatroomUser(chatroomUser).then(
+    chatroomUser => dispatch(receiveChatroomUser(chatroomUser)),
+    errors => dispatch(receiveChatroomErrors(errors))
+  )
+);
--- frontend/reducers/entities/chatroom_users_reducer.js
+++ frontend/reducers/entities/chatroom_users_reducer.js
@@ -4,11 +4,15 @@ import {
 import {
   RECEIVE_CHATROOMS
 } from '../../actions/chatroom_actions';
-
+import {
+  RECEIVE_CHATROOM_USER
+} from '../../actions/chatroom_user_actions';
 
 const chatroomUsersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_CHATROOM_USER:
+      return merge({}, state, action.chatroomUser);
     case RECEIVE_CHATROOMS:
       return merge({}, state, action.payload.chatroomUsers);
     default:
