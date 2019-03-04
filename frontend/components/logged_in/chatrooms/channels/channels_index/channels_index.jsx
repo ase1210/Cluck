ebc052ca2bd44619201fe6e73f1c78451e4d1768
DrewE <andrew.engelstein@gmail.com>
2019-03-03 17:11:43 -0800

    Add conditional rendering for join channel form based on search query

--- frontend/components/logged_in/chatrooms/channels/channels_index/channels_index.jsx
+++ frontend/components/logged_in/chatrooms/channels/channels_index/channels_index.jsx
@@ -3,17 +3,38 @@ import ChannelIndexItem from './channel_index_item';
 
 const ChannelsIndex = (props) => (
   <>
-    <div>
-      <p>Channels you can join</p>
-    </div>
+    {
+      props.channels.available.length === 0 && props.channels.subscribed.length === 0
+        ?
+        <div>
+          <p>No matches found for <span style={{ fontWeight: 700 }}>{`${props.search}`}</span></p>
+        </div>
+        :
+        <></>
+    }
+    {
+      props.channels.available.length !== 0
+        ?
+        <div>
+          <p>Channels you can join</p>
+        </div>
+        :
+        <></>
+    }
     {props.channels.available.map(channel =>
       <div key={channel.id} >
         <ChannelIndexItem channel={channel} formatDate={props.formatDate} />
       </div>
     )}
-    <div>
-      <p>Channels you belong to</p>
-    </div>
+    {
+      props.channels.subscribed.length !== 0
+        ?
+        <div>
+          <p>Channels you belong to</p>
+        </div>
+        :
+        <></>
+    }
     {props.channels.subscribed.map(channel =>
       <div key={channel.id} >
         <ChannelIndexItem channel={channel} formatDate={props.formatDate} />
