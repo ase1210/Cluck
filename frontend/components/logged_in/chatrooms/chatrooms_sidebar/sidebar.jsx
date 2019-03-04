439375315ca471924c8758ed98b4370acc08d43e
DrewE <andrew.engelstein@gmail.com>
2019-03-04 14:45:27 -0800

    Update structure and styling for logged in Sidebar component

--- frontend/components/logged_in/chatrooms/chatrooms_sidebar/sidebar.jsx
+++ frontend/components/logged_in/chatrooms/chatrooms_sidebar/sidebar.jsx
@@ -37,44 +37,46 @@ class Sidebar extends React.Component {
 
   render() {
     return (
-      <div className='sidebar'>
+      <div className='sidebar-parent'>
         <div className='sidebar-nav'>
           <div className='app-title'>Cluck</div>
           <div>
             <NavBarContainer />
           </div>
         </div>
-        <div className='presentational'></div>
-        <div className='sidebar-data'>
+        <div className='sidebar'>
           <div className='presentational'></div>
-          <div className='section-header' onClick={this.routeChange('join')}>Channels</div>
-          {this.props.chatrooms.channels.map(channel => {
-            let klass = (channel.id === parseInt(this.props.match.params.chatroomId)) ?
-              "selected" : "chatroom";
-            return (
-              <div className={`${klass}`} key={channel.id} >
-                <Link to={`/messages/${channel.id}`}>
-                  <p># {channel.name}</p>
-                </Link>
-              </div>)
-          })}
-          <div className='presentational'></div>
-          <div className='section-header' onClick={this.routeChange('new')}>+ Add a channel</div>
-          <div className='presentational'></div>
-          <div className='section-header'>Direct Messages</div>
-          {this.props.chatrooms.directMessages.map(directMessage => {
-            let klass = (directMessage.id === parseInt(this.props.match.params.chatroomId)) ?
-              "selected" : "chatroom";
-            return (
-              <div className={`${klass}`} key={directMessage.id} >
-                <Link to={`/messages/${directMessage.id}`}>
-                  <p># {directMessage.name}</p>
-                </Link>
-              </div>)
-          })}
-          <div className='presentational'></div>
-          <div className='presentational'></div>
-          <div className='presentational'></div>
+          <div className='sidebar-data'>
+            <div className='presentational'></div>
+            <div className='section-header' onClick={this.routeChange('join')}>Channels</div>
+            {this.props.chatrooms.channels.map(channel => {
+              let klass = (channel.id === parseInt(this.props.match.params.chatroomId)) ?
+                "selected" : "chatroom";
+              return (
+                <div className={`${klass}`} key={channel.id} >
+                  <Link to={`/messages/${channel.id}`}>
+                    <p># {channel.name}</p>
+                  </Link>
+                </div>)
+            })}
+            <div className='presentational'></div>
+            <div className='section-header' onClick={this.routeChange('new')}>+ Add a channel</div>
+            <div className='presentational'></div>
+            <div className='section-header'>Direct Messages</div>
+            {this.props.chatrooms.directMessages.map(directMessage => {
+              let klass = (directMessage.id === parseInt(this.props.match.params.chatroomId)) ?
+                "selected" : "chatroom";
+              return (
+                <div className={`${klass}`} key={directMessage.id} >
+                  <Link to={`/messages/${directMessage.id}`}>
+                    <p># {directMessage.name}</p>
+                  </Link>
+                </div>)
+            })}
+            <div className='presentational'></div>
+            <div className='presentational'></div>
+            <div className='presentational'></div>
+          </div>
         </div>
       </div>
     )
