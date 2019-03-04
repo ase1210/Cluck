9a65af84297ec655d328fd478311e02af68a7b33
DrewE <andrew.engelstein@gmail.com>
2019-03-03 16:38:05 -0800

    Add state to JoinChannelForm and set to update onChange of search input field

--- frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
+++ frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import ChannelsIndexContainer from '../channels_index/channels_index_container';
 
 class JoinChannelForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: "" };
+    this.handleInput = this.handleInput.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchChatrooms();
+  }
+
+  handleInput(e) {
+    this.setState({ search: e.target.value });
   }
 
   render() {
     return (
       <div>
         <h2>Browse Channels</h2>
-        <input type="text" placeholder='Search channels' />
-        <ChannelsIndexContainer />
+        <input type="text" placeholder='Search channels' onChange={this.handleInput} />
+        <ChannelsIndexContainer search={this.state.search} />
       </div>
     )
   }
