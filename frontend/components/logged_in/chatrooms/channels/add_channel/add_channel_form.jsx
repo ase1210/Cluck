d5ff5e6cf59c794776d19d396e690017c2d36083
DrewE <andrew.engelstein@gmail.com>
2019-03-03 22:59:25 -0800

    Add content to Add Channel form

--- frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form.jsx
+++ frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
 class AddChannelForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      channel: true,
+      admin_id: props.currentUser,
+    };
+  }
+
+  renderErrors() {
+
+    // <p>Don't forget to name your channel.</p>
+    // <p>INPUT VALUE is already taken by a channel, username, or user group.</p>
+    // <p>Channel names can't contain spaces, periods, or most punctuation. Try again?</p>
+    // <p>Blerg, that's a bit too long! Channel name must be fewer than 22 characters.</p>
+  }
 
   render() {
     return (
       <div>
-        <h2>Add Channel Form</h2>
+        <h2>Create a channel</h2>
+        <p>Channels are where your friends communicate.  They're best when organized around a topic - #music, for example.</p>
+        <form>
+          <label>Name
+            <br />
+            <input type="text" placeholder='e.g. music' />
+            <p>Names must be lowercase, without spaces or periods, and shorter than 22 characters.</p>
+          </label>
+        </form>
       </div>
     )
   }
