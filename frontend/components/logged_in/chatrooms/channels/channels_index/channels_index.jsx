b16e334ea6efd4363513f4d4c8eb8d6db591dea1
DrewE <andrew.engelstein@gmail.com>
2019-03-03 17:20:26 -0800

    Reformat ChannelIndex component to a class component

--- frontend/components/logged_in/chatrooms/channels/channels_index/channels_index.jsx
+++ frontend/components/logged_in/chatrooms/channels/channels_index/channels_index.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
 import ChannelIndexItem from './channel_index_item';
 
-const ChannelsIndex = (props) => (
-  <>
-    {
-      props.channels.available.length === 0 && props.channels.subscribed.length === 0
-        ?
-        <div>
-          <p>No matches found for <span style={{ fontWeight: 700 }}>{`${props.search}`}</span></p>
-        </div>
-        :
-        <></>
-    }
-    {
-      props.channels.available.length !== 0
-        ?
-        <div>
-          <p>Channels you can join</p>
-        </div>
-        :
-        <></>
-    }
-    {props.channels.available.map(channel =>
-      <div key={channel.id} >
-        <ChannelIndexItem channel={channel} formatDate={props.formatDate} />
-      </div>
-    )}
-    {
-      props.channels.subscribed.length !== 0
-        ?
-        <div>
-          <p>Channels you belong to</p>
-        </div>
-        :
-        <></>
-    }
-    {props.channels.subscribed.map(channel =>
-      <div key={channel.id} >
-        <ChannelIndexItem channel={channel} formatDate={props.formatDate} />
-      </div>
-    )}
-  </>
-)
+class ChannelsIndex extends React.Component {
+
+  render() {
+    return (
+      <>
+        {
+          this.props.channels.available.length === 0 && this.props.channels.subscribed.length === 0
+            ?
+            <div>
+              <p>No matches found for <span style={{ fontWeight: 700 }}>{`${this.props.search}`}</span></p>
+            </div>
+            :
+            <></>
+        }
+        {
+          this.props.channels.available.length !== 0
+            ?
+            <div>
+              <p>Channels you can join</p>
+            </div>
+            :
+            <></>
+        }
+        {this.props.channels.available.map(channel =>
+          <div key={channel.id} >
+            <ChannelIndexItem channel={channel} formatDate={this.props.formatDate} />
+          </div>
+        )}
+        {
+          this.props.channels.subscribed.length !== 0
+            ?
+            <div>
+              <p>Channels you belong to</p>
+            </div>
+            :
+            <></>
+        }
+        {this.props.channels.subscribed.map(channel =>
+          <div key={channel.id} >
+            <ChannelIndexItem channel={channel} formatDate={this.props.formatDate} />
+          </div>
+        )}
+      </>
+    )
+  }
+}
+
 
 export default ChannelsIndex;
