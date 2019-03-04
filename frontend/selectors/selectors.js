65cc2425f2bf538827391cb5e93c3d31874821ce
DrewE <andrew.engelstein@gmail.com>
2019-03-03 20:39:36 -0800

    Update selectChatrooms to only return current user chatrooms

--- frontend/selectors/selectors.js
+++ frontend/selectors/selectors.js
@@ -21,18 +21,24 @@ export const selectChannels = (state, str) => {
 };
 
 export const selectChatrooms = (state) => {
+  const currentUser = parseInt(state.session.currentUser);
+  let userChatroomIds = Object.values(state.entities.chatroomUsers)
+    .filter(chatroomUser => chatroomUser.userId === currentUser)
+    .map(chatroomUser => chatroomUser.chatroomId);
   let chatrooms = Object.values(state.entities.chatrooms);
   let channels = [];
   let directMessages = [];
   let generalChatroomId;
   chatrooms.forEach(chatroom => {
-    if (chatroom.channel) {
-      if (chatroom.name === 'general') {
-        generalChatroomId = chatroom.id;
+    if (userChatroomIds.includes(chatroom.id)) {
+      if (chatroom.channel) {
+        if (chatroom.name === 'general') {
+          generalChatroomId = chatroom.id;
+        }
+        channels.push(chatroom);
+      } else {
+        directMessages.push(chatroom);
       }
-      channels.push(chatroom);
-    } else {
-      directMessages.push(chatroom);
     }
   });
   return {
@@ -45,29 +51,3 @@ export const selectChatrooms = (state) => {
 export const selectChatroomIds = (state) => (
   Object.keys(state.entities.chatrooms)
 );
-
-
-
-
-// export const selectChannels = (state, str) => {
-//   const currentUser = parseInt(state.session.currentUser);
-//   let channels = Object.values(state.entities.chatrooms)
-//     .filter(chatroom => chatroom.channel);
-//   let userChatroomIds = Object.values(state.entities.chatroomUsers)
-//     .filter(chatroomUser => chatroomUser.userId === currentUser)
-//     .map(chatroomUser => chatroomUser.chatroomId);
-//   // debugger
-//   let subscribed = [];
-//   let available = [];
-//   channels.forEach(channel => {
-//     if (userChatroomIds.includes(channel.id)) {
-//       subscribed.push(channel);
-//     } else {
-//       available.push(channel);
-//     }
-//   });
-//   return {
-//     subscribed,
-//     available
-//   };
-// };
