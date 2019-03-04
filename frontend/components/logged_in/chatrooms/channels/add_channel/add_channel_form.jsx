65a105dd35c47a7483315a924013d886939b570c
DrewE <andrew.engelstein@gmail.com>
2019-03-04 10:47:54 -0800

    Add functionality to AddChannelForm cancel button and input field

--- frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form.jsx
+++ frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form.jsx
@@ -7,7 +7,10 @@ class AddChannelForm extends React.Component {
       name: "",
       channel: true,
       admin_id: props.currentUser,
+      user_ids: [props.currentUser]
     };
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   renderErrors() {
@@ -16,6 +19,22 @@ class AddChannelForm extends React.Component {
     // <p>INPUT VALUE is already taken by a channel, username, or user group.</p>
     // <p>Channel names can't contain spaces, periods, or most punctuation. Try again?</p>
     // <p>Blerg, that's a bit too long! Channel name must be fewer than 22 characters.</p>
+  }
+
+  handleInput(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleCancelClick(e) {
+    e.preventDefault();
+    let chatroomId = this.props.match.params.chatroomId;
+    let path = `/messages/${chatroomId}`;
+    this.props.history.push(path)
+  }
+
+  handleCreateChannel(e) {
+    e.preventDefault();
+    this.props.createChatroom(this.state);
   }
 
   render() {
@@ -27,13 +46,13 @@ class AddChannelForm extends React.Component {
           <form>
             <label>Name
             <br />
-              <input type="text" placeholder='e.g. music' />
+              <input type="text" placeholder='e.g. music' autoFocus onChange={this.handleInput} />
               <p>Names must be lowercase, without spaces or periods, and shorter than 22 characters.</p>
             </label>
             <div className='buttons'>
 
-              <button>Cancel</button>
-              <button className='create'>Create Channel</button>
+              <button onClick={this.handleCancelClick}>Cancel</button>
+              <button className='create' onClick={this.handleCreateChannel}>Create Channel</button>
             </div>
           </form>
         </div>
--- frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form_container.jsx
+++ frontend/components/logged_in/chatrooms/channels/add_channel/add_channel_form_container.jsx
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
 import AddChannelForm from './add_channel_form';
+import { createChatroom } from '../../../../../actions/chatroom_actions';
 
 
 const mDTP = (dispatch) => ({
+  createChatroom: (chatroom) => dispatch(createChatroom(chatroom))
 });
 
 const AddChannelFormContainer = connect(undefined, mDTP)(AddChannelForm);
