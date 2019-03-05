9fd6a13efb71b32df6c55cc7c5c5af7b191e0554
DrewE <andrew.engelstein@gmail.com>
2019-03-04 17:25:35 -0800

    Add escape button with formatting to Join Channel form

--- frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
+++ frontend/components/logged_in/chatrooms/channels/join_channels/join_channel_form.jsx
@@ -6,6 +6,7 @@ class JoinChannelForm extends React.Component {
     super(props);
     this.state = { search: "" };
     this.handleInput = this.handleInput.bind(this);
+    this.handleEscape = this.handleEscape.bind(this);
   }
 
   componentDidMount() {
@@ -16,12 +17,19 @@ class JoinChannelForm extends React.Component {
     this.setState({ search: e.target.value });
   }
 
+  handleEscape() {
+    let chatroomId = this.props.match.params.chatroomId;
+    let path = `/messages/${chatroomId}`;
+    this.props.history.push(path);
+  }
+
   render() {
     return (
       <div className='jcf-parent'>
         <div className='join-channel-form'>
           <div className='jcf-title'>
             <h1>Browse channels</h1>
+            <div className='escape-form' onClick={this.handleEscape}>x</div>
           </div>
           <div className='jcf-search'>
             <input type="text" placeholder='Search channels' onChange={this.handleInput} />
