82c4205c32f49bd8103912470fdfeb97450af668
DrewE <andrew.engelstein@gmail.com>
2019-03-05 16:04:52 -0800

    Finalize Message styling

--- frontend/components/logged_in/chatrooms/messages/messages.jsx
+++ frontend/components/logged_in/chatrooms/messages/messages.jsx
@@ -3,84 +3,9 @@ import React from 'react';
 const Messages = (props) => (
   <div className="messages-parent">
     <header>
-      <p>CHATROOM NAME</p>
+      <p># CHATROOM NAME</p>
     </header>
     <section className='message-area'>
-      <div className='message-data'>
-        <div className='message-image'>
-          <img src='/images/cluck-logo.png' alt-text='profile-image' />
-        </div>
-        <div className='message-content-area'>
-
-          <div className='message-info'>
-            <span className='mi-username'>USERNAME</span>
-            <span className='mi-time'>MESSAGE TIME w. Date</span>
-          </div>
-          <div className='message-content'>
-            <p>MESSAGE CONTENT</p>
-          </div>
-        </div>
-      </div>
-      <div className='message-data'>
-        <div className='message-image'>
-          <img src='/images/cluck-logo.png' alt-text='profile-image' />
-        </div>
-        <div className='message-content-area'>
-
-          <div className='message-info'>
-            <span className='mi-username'>USERNAME</span>
-            <span className='mi-time'>MESSAGE TIME w. Date</span>
-          </div>
-          <div className='message-content'>
-            <p>MESSAGE CONTENT</p>
-          </div>
-        </div>
-      </div>
-      <div className='message-data'>
-        <div className='message-image'>
-          <img src='/images/cluck-logo.png' alt-text='profile-image' />
-        </div>
-        <div className='message-content-area'>
-
-          <div className='message-info'>
-            <span className='mi-username'>USERNAME</span>
-            <span className='mi-time'>MESSAGE TIME w. Date</span>
-          </div>
-          <div className='message-content'>
-            <p>MESSAGE CONTENT</p>
-          </div>
-        </div>
-      </div>
-      <div className='message-data'>
-        <div className='message-image'>
-          <img src='/images/cluck-logo.png' alt-text='profile-image' />
-        </div>
-        <div className='message-content-area'>
-
-          <div className='message-info'>
-            <span className='mi-username'>USERNAME</span>
-            <span className='mi-time'>MESSAGE TIME w. Date</span>
-          </div>
-          <div className='message-content'>
-            <p>MESSAGE CONTENT</p>
-          </div>
-        </div>
-      </div>
-      <div className='message-data'>
-        <div className='message-image'>
-          <img src='/images/cluck-logo.png' alt-text='profile-image' />
-        </div>
-        <div className='message-content-area'>
-
-          <div className='message-info'>
-            <span className='mi-username'>USERNAME</span>
-            <span className='mi-time'>MESSAGE TIME w. Date</span>
-          </div>
-          <div className='message-content'>
-            <p>MESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENTMESSAGE CONTENT</p>
-          </div>
-        </div>
-      </div>
       <div className='message-data'>
         <div className='message-image'>
           <img src='/images/cluck-logo.png' alt-text='profile-image' />
