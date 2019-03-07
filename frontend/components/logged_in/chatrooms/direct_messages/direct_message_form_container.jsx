e5564fe23e981b495fc122213803c000696a403e
DrewE <andrew.engelstein@gmail.com>
2019-03-07 14:44:26 -0800

    Add DirectMessageFormContainer and update route references to this component

---
+++ frontend/components/logged_in/chatrooms/direct_messages/direct_message_form_container.jsx
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import DirectMessageForm from './direct_message_form';
+import { fetchAllUsers } from '../../../../actions/session_actions';
+
+const mDTP = (dispatch) => ({
+  fetchAllUsers: () => dispatch(fetchAllUsers())
+});
+
+const DirectMessageFormContainer = connect(undefined, mDTP)(DirectMessageForm);
+
+export default DirectMessageFormContainer;
--- frontend/components/logged_in/logged_in_app.jsx
+++ frontend/components/logged_in/logged_in_app.jsx
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import JoinChannelFormContainer from './chatrooms/channels/join_channels/join_channel_form_container';
 import AddChannelFormContainer from './chatrooms/channels/add_channel/add_channel_form_container';
 import MainView from './chatrooms/main_view';
-import DirectMessageForm from './chatrooms/direct_messages/direct_message_form';
+import DirectMessageFormContainer from './chatrooms/direct_messages/direct_message_form_container';
 
 const LoggedInApp = (props) => (
   <div>
@@ -11,7 +11,7 @@ const LoggedInApp = (props) => (
       <Route exact path='/messages/:chatroomId' component={MainView} />
       <Route exact path='/messages/:chatroomId/new' component={AddChannelFormContainer} />
       <Route exact path='/messages/:chatroomId/join' component={JoinChannelFormContainer} />
-      <Route exact path='/messages/:chatroomId/dm' component={DirectMessageForm} />
+      <Route exact path='/messages/:chatroomId/dm' component={DirectMessageFormContainer} />
       <Redirect to='/messages/1' />
     </Switch>
   </div>
