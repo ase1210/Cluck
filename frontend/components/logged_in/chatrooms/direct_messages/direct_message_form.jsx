243658f413eea1703726827a072a0f491c2a7acb
DrewE <andrew.engelstein@gmail.com>
2019-03-07 14:35:45 -0800

    Add direct message form basic component with sample html and styling

---
+++ frontend/components/logged_in/chatrooms/direct_messages/direct_message_form.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class DirectMessageForm extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { search: "" };
+  //   this.handleInput = this.handleInput.bind(this);
+  //   this.handleEscape = this.handleEscape.bind(this);
+  // }
+
+  // componentDidMount() {
+  //   this.props.fetchChatrooms();
+  // }
+
+  // handleInput(e) {
+  //   this.setState({ search: e.target.value });
+  // }
+
+  // handleEscape() {
+  //   let chatroomId = this.props.match.params.chatroomId;
+  //   let path = `/messages/${chatroomId}`;
+  //   this.props.history.push(path);
+  // }
+
+  render() {
+    return (
+      <div className='dmf-parent'>
+        <div className='direct-message-form'>
+          <div className='dmf-title'>
+            <h1>Direct Messages</h1>
+            <div className='escape-form' >x</div>
+          </div>
+          <div className='dmf-search'>
+            <input type="text" placeholder='Find or start a conversation' />
+          </div>
+          <div className='presentational'></div>
+          <div className='dmf-user-browser-container'>
+            <div className='dmf-ubc-user'>
+              <div className='dmf-ubc-user-image'>IMG</div>
+              <div className='dmf-ubc-user-info'>
+                <span>USERNAME</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default DirectMessageForm;
--- frontend/components/logged_in/logged_in_app.jsx
+++ frontend/components/logged_in/logged_in_app.jsx
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import JoinChannelFormContainer from './chatrooms/channels/join_channels/join_channel_form_container';
 import AddChannelFormContainer from './chatrooms/channels/add_channel/add_channel_form_container';
 import MainView from './chatrooms/main_view';
+import DirectMessageForm from './chatrooms/direct_messages/direct_message_form';
 
 const LoggedInApp = (props) => (
   <div>
@@ -10,6 +11,7 @@ const LoggedInApp = (props) => (
       <Route exact path='/messages/:chatroomId' component={MainView} />
       <Route exact path='/messages/:chatroomId/new' component={AddChannelFormContainer} />
       <Route exact path='/messages/:chatroomId/join' component={JoinChannelFormContainer} />
+      <Route exact path='/messages/:chatroomId/dm' component={DirectMessageForm} />
       <Redirect to='/messages/1' />
     </Switch>
   </div>
