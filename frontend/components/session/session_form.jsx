135761ee8f2dc01151f67c7d435053d4576fc39a
DrewE <andrew.engelstein@gmail.com>
2019-04-04 12:30:19 -0700

    Add demo login ghosting

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import NavBarContainer from '../navbar/navbar_container';
+import React from "react";
+import NavBarContainer from "../navbar/navbar_container";
 
-const SessionErrors = (props) => (
+const SessionErrors = props => (
   <div className="session-errors-container">
     {props.sessionErrors.map((error, idx) => (
-      <p className="session-errors-item" key={idx}>{error}</p>
-    )
-    )}
+      <p className="session-errors-item" key={idx}>
+        {error}
+      </p>
+    ))}
   </div>
-)
+);
 
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      form: { username: "", password: "" },
-      demo: { username: 'Drew', password: 'tester' }
+      username: "",
+      password: ""
+      // form: { username: "", password: "" },
+      // demo: { username: "Drew", password: "tester" }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDemoSubmit = this.handleDemoSubmit.bind(this);
@@ -26,50 +29,84 @@ class SessionForm extends React.Component {
   }
 
   handleInput(field) {
-    return (e) => {
-      const form = this.state.form;
-      form[field] = e.target.value;
-      this.setState({ form });
+    return e => {
+      // const form = this.state.form;
+      // form[field] = e.target.value;
+      this.setState({ [field]: e.target.value });
     };
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.action(this.state.form);
+    this.props.action(this.state);
   }
 
   handleDemoSubmit(e) {
     e.preventDefault();
-    this.props.demoAction(this.state.demo);
+    const un = "Drew".split("");
+    const pw = "tester".split("");
+    this.setState({ username: "", password: "" }, () =>
+      this.demoSignInHelper(un, pw)
+    );
+  }
+
+  demoSignInHelper(un, pw) {
+    if (un.length > 0) {
+      this.setState({ username: this.state.username + un.shift() }, () => {
+        window.setTimeout(() => this.demoSignInHelper(un, pw), 60);
+      });
+    } else if (pw.length > 0) {
+      this.setState({ password: this.state.password + pw.shift() }, () => {
+        window.setTimeout(() => this.demoSignInHelper(un, pw), 60);
+      });
+    } else {
+      this.props.action(this.state);
+    }
   }
 
   render() {
     return (
       <div>
         <NavBarContainer />
-        <div className='session-form-parent'>
-          {this.props.sessionErrors.length === 0 ? <></> : <SessionErrors sessionErrors={this.props.sessionErrors} />}
+        <div className="session-form-parent">
+          {this.props.sessionErrors.length === 0 ? (
+            <></>
+          ) : (
+            <SessionErrors sessionErrors={this.props.sessionErrors} />
+          )}
           <div className="session-form-container">
-            <h2>{this.props.formType} {this.props.formType === "Sign up" ? "for" : "to"} Cluck</h2>
+            <h2>
+              {this.props.formType}{" "}
+              {this.props.formType === "Sign up" ? "for" : "to"} Cluck
+            </h2>
             <form>
-              <p>Enter your <span style={{ fontWeight: 'bold' }}>username</span> and <span style={{ fontWeight: 'bold' }}>password</span>.</p>
-              <input type='text' value={this.state.username}
+              <p>
+                Enter your <span style={{ fontWeight: "bold" }}>username</span>{" "}
+                and <span style={{ fontWeight: "bold" }}>password</span>.
+              </p>
+              <input
+                type="text"
+                value={this.state.username}
                 autoFocus="autofocus"
-                placeholder='username'
-                onChange={this.handleInput('username')} />
+                placeholder="username"
+                onChange={this.handleInput("username")}
+              />
               <br />
-              <input type='password' value={this.state.password}
-                placeholder='password'
-                onChange={this.handleInput('password')} />
+              <input
+                type="password"
+                value={this.state.password}
+                placeholder="password"
+                onChange={this.handleInput("password")}
+              />
               <br />
               <button onClick={this.handleSubmit}>{this.props.formType}</button>
               <br />
               <button onClick={this.handleDemoSubmit}>Demo Login</button>
             </form>
           </div>
-        </div >
+        </div>
       </div>
-    )
+    );
   }
 }
 
