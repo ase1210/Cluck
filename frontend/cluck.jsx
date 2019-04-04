e46fedf9d0e7c2832d24ecc85bf5c29d4eaf4cb2
DrewE <andrew.engelstein@gmail.com>
2019-04-04 16:05:13 -0700

    Update entry file pre-loaded state to include selectedUsers in session slice

--- frontend/cluck.jsx
+++ frontend/cluck.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
   let preloadedState;
   if (window.currentUser) {
     preloadedState = {
-      session: { currentUser: Object.keys(window.currentUser)[0] },
+      session: {
+        currentUser: Object.keys(window.currentUser)[0],
+        selectedUsers: []
+      },
       entities: { users: window.currentUser }
     };
   }
   const store = configureStore(preloadedState);
   delete window.currentUser;
-  ReactDOM.render(<Root store={store} />, root)
+  ReactDOM.render(<Root store={store} />, root);
 });
