cd9ccd902c76ac385b3a28ad9e4c5f988b1738d0
DrewE <andrew.engelstein@gmail.com>
2019-04-05 11:55:49 -0700

    Add #postDMChatroom to chatroom actions

--- frontend/actions/chatroom_actions.js
+++ frontend/actions/chatroom_actions.js
@@ -1,29 +1,28 @@
-import * as ChatroomAPIUtil from '../util/chatroom_api_util';
+import * as ChatroomAPIUtil from "../util/chatroom_api_util";
 
 export const RECEIVE_CHATROOMS = "RECEIVE_CHATROOMS";
 export const RECEIVE_CHATROOM = "RECEIVE_CHATROOM";
 export const RECEIVE_CHATROOM_ERRORS = "RECEIVE_CHATROOM_ERRORS";
 
-const receiveChatrooms = (payload) => ({
+const receiveChatrooms = payload => ({
   type: RECEIVE_CHATROOMS,
   payload
 });
 
-const receiveChatroom = (payload) => ({
+const receiveChatroom = payload => ({
   type: RECEIVE_CHATROOM,
   payload
 });
 
-const receiveChatroomErrors = (errors) => ({
+const receiveChatroomErrors = errors => ({
   type: RECEIVE_CHATROOM_ERRORS,
   errors
 });
 
-export const fetchChatrooms = () => dispatch => (
-  ChatroomAPIUtil.getChatrooms().then(
-    chatrooms => dispatch(receiveChatrooms(chatrooms))
-  )
-);
+export const fetchChatrooms = () => dispatch =>
+  ChatroomAPIUtil.getChatrooms().then(chatrooms =>
+    dispatch(receiveChatrooms(chatrooms))
+  );
 
 // export const fetchChatroom = (id) => dispatch => (
 //   ChatroomAPIUtil.getChatroom(id).then(
@@ -31,9 +30,14 @@ export const fetchChatrooms = () => dispatch => (
 //   )
 // );
 
-export const createChatroom = (chatroom) => dispatch => (
+export const createChatroom = chatroom => dispatch =>
   ChatroomAPIUtil.postChatroom(chatroom).then(
     chatroom => dispatch(receiveChatroom(chatroom)),
     errors => dispatch(receiveChatroomErrors(errors))
-  )
-);
+  );
+
+export const postDMChatroom = chatroom => dispatch =>
+  ChatroomAPIUtil.postDMChatroom(chatroom).then(
+    chatroom => dispatch(receiveChatroom(chatroom)),
+    errors => dispatch(receiveChatroomErrors(errors))
+  );
