c4e67a9d4c46965a1e20e68c67cd79b5aeee6ffb
DrewE <andrew.engelstein@gmail.com>
2019-04-05 14:55:29 -0700

    Add currentUser to mSTP

--- frontend/components/logged_in/chatrooms/direct_messages/add_direct_message/direct_message_form_container.jsx
+++ frontend/components/logged_in/chatrooms/direct_messages/add_direct_message/direct_message_form_container.jsx
@@ -2,14 +2,17 @@ import { connect } from "react-redux";
 import DirectMessageForm from "./direct_message_form";
 import { fetchAllUsers } from "../../../../../actions/session_actions";
 import { removeUserSelection } from "../../../../../actions/dm_actions";
+import { createDMChatroom } from "../../../../../actions/chatroom_actions";
 
 const mSTP = state => ({
-  selectedUsers: Object.values(state.ui.selectedUsers)
+  selectedUsers: Object.values(state.ui.selectedUsers),
+  currentUser: state.entities.users[state.session.currentUser]
 });
 
 const mDTP = dispatch => ({
   fetchAllUsers: () => dispatch(fetchAllUsers()),
-  removeUserSelection: id => dispatch(removeUserSelection(id))
+  removeUserSelection: id => dispatch(removeUserSelection(id)),
+  createDMChatroom: chatroom => dispatch(createDMChatroom(chatroom))
 });
 
 const DirectMessageFormContainer = connect(
