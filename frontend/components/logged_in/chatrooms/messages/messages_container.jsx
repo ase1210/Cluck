bb59a698170751acb3d13154f11c740f3bfdc349
DrewE <andrew.engelstein@gmail.com>
2019-04-08 14:32:18 -0700

    Add getDMName and currentUserName to mSTP

--- frontend/components/logged_in/chatrooms/messages/messages_container.jsx
+++ frontend/components/logged_in/chatrooms/messages/messages_container.jsx
@@ -1,22 +1,31 @@
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import Messages from './messages';
-import { createMessage } from '../../../../actions/message_actions';
-
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Messages from "./messages";
+import { createMessage } from "../../../../actions/message_actions";
+import { getDMName } from "../../../../selectors/chatrooms_selectors";
 
 const mSTP = (state, ownProps) => {
   let chatroomId = parseInt(ownProps.match.params.chatroomId);
   let activeChatroom = state.entities.chatrooms[chatroomId];
   let currentUser = parseInt(state.session.currentUser);
-  return ({
-    activeChatroom, currentUser
-  });
+  let currentUserName = state.entities.users[currentUser].username;
+  return {
+    activeChatroom,
+    currentUser,
+    currentUserName,
+    getDMName
+  };
 };
 
-const mDTP = (dispatch) => ({
-  createMessage: (message) => dispatch(createMessage(message))
+const mDTP = dispatch => ({
+  createMessage: message => dispatch(createMessage(message))
 });
 
-const MessagesContainer = withRouter(connect(mSTP, mDTP)(Messages));
+const MessagesContainer = withRouter(
+  connect(
+    mSTP,
+    mDTP
+  )(Messages)
+);
 
 export default MessagesContainer;
