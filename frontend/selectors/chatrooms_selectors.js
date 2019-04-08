c95706a43712d7da72c4e9c27aede4dbe28833e8
DrewE <andrew.engelstein@gmail.com>
2019-04-08 14:18:07 -0700

    Add #getDMName function to remove current users name from DM chatroom name

--- frontend/selectors/chatrooms_selectors.js
+++ frontend/selectors/chatrooms_selectors.js
@@ -1,9 +1,14 @@
 export const selectChannels = (state, str) => {
   const currentUser = parseInt(state.session.currentUser);
-  let channels = Object.values(state.entities.chatrooms)
-    .filter(chatroom => chatroom.channel && chatroom.name.startsWith(str.toLowerCase()));
+  let channels = Object.values(state.entities.chatrooms).filter(
+    chatroom => chatroom.channel && chatroom.name.startsWith(str.toLowerCase())
+  );
   let userChatroomIds = Object.values(state.entities.chatroomUsers)
-    .filter(chatroomUser => chatroomUser.userId === currentUser && ["active", "invited", 'viewing'].includes(chatroomUser.status))
+    .filter(
+      chatroomUser =>
+        chatroomUser.userId === currentUser &&
+        ["active", "invited", "viewing"].includes(chatroomUser.status)
+    )
     .map(chatroomUser => chatroomUser.chatroomId);
   let subscribed = [];
   let available = [];
@@ -22,10 +27,14 @@ export const selectChannels = (state, str) => {
   };
 };
 
-export const selectChatrooms = (state) => {
+export const selectChatrooms = state => {
   const currentUser = parseInt(state.session.currentUser);
   let userChatroomIds = Object.values(state.entities.chatroomUsers)
-    .filter(chatroomUser => chatroomUser.userId === currentUser && ["active", "invited", 'viewing'].includes(chatroomUser.status))
+    .filter(
+      chatroomUser =>
+        chatroomUser.userId === currentUser &&
+        ["active", "invited", "viewing"].includes(chatroomUser.status)
+    )
     .map(chatroomUser => chatroomUser.chatroomId);
   let chatrooms = Object.values(state.entities.chatrooms);
   let channels = [];
@@ -34,7 +43,7 @@ export const selectChatrooms = (state) => {
   chatrooms.forEach(chatroom => {
     if (userChatroomIds.includes(chatroom.id)) {
       if (chatroom.channel) {
-        if (chatroom.name === 'general') {
+        if (chatroom.name === "general") {
           generalChatroomId = chatroom.id;
         }
         channels.push(chatroom);
@@ -44,7 +53,9 @@ export const selectChatrooms = (state) => {
     }
   });
   channels.sort((a, b) => (a.name < b.name ? -1 : 1));
-  directMessages.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1));
+  directMessages.sort((a, b) =>
+    a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+  );
 
   return {
     channels,
@@ -53,21 +64,31 @@ export const selectChatrooms = (state) => {
   };
 };
 
-export const selectChatroomIds = (state) => (
-  Object.keys(state.entities.chatrooms)
-);
+export const selectChatroomIds = state => Object.keys(state.entities.chatrooms);
 
-export const selectUserChatroomIds = (state) => {
+export const selectUserChatroomIds = state => {
   const currentUser = parseInt(state.session.currentUser);
   let userChatroomIds = Object.values(state.entities.chatroomUsers)
     .filter(chatroomUser => chatroomUser.userId === currentUser)
     .map(chatroomUser => chatroomUser.chatroomId);
   return userChatroomIds;
 };
-export const selectSubscribedUserChatroomIds = (state) => {
+export const selectSubscribedUserChatroomIds = state => {
   const currentUser = parseInt(state.session.currentUser);
   let userChatroomIds = Object.values(state.entities.chatroomUsers)
-    .filter(chatroomUser => chatroomUser.userId === currentUser && ["active", "invited", 'viewing'].includes(chatroomUser.status))
+    .filter(
+      chatroomUser =>
+        chatroomUser.userId === currentUser &&
+        ["active", "invited", "viewing"].includes(chatroomUser.status)
+    )
     .map(chatroomUser => chatroomUser.chatroomId);
   return userChatroomIds;
+};
+
+export const getDMName = (name, nameToDelete) => {
+  console.log(name, nameToDelete);
+  let names = name.split(", ");
+  let idx = names.indexOf(nameToDelete);
+  let newNames = names.slice(0, idx).concat(names.slice(idx + 1));
+  return newNames.join(", ");
 };
