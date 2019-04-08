3d75488f50c1f24ada79254633497bdbfc3395b9
DrewE <andrew.engelstein@gmail.com>
2019-04-08 13:40:52 -0700

    Add fetchChatroom to mDTP in sidebarContainer

--- frontend/components/logged_in/chatrooms/chatrooms_sidebar/sidebar_container.jsx
+++ frontend/components/logged_in/chatrooms/chatrooms_sidebar/sidebar_container.jsx
@@ -1,12 +1,19 @@
-import { connect } from 'react-redux';
-import Sidebar from './sidebar';
-import { withRouter } from 'react-router-dom'
-import { selectChatrooms, selectChatroomIds, selectSubscribedUserChatroomIds } from '../../../../selectors/chatrooms_selectors';
-import { fetchChatrooms } from '../../../../actions/chatroom_actions';
-import { updateChatroomUser } from '../../../../actions/chatroom_user_actions';
-import { receiveMessage } from '../../../../actions/message_actions';
+import { connect } from "react-redux";
+import Sidebar from "./sidebar";
+import { withRouter } from "react-router-dom";
+import {
+  selectChatrooms,
+  selectChatroomIds,
+  selectSubscribedUserChatroomIds
+} from "../../../../selectors/chatrooms_selectors";
+import {
+  fetchChatrooms,
+  fetchChatroom
+} from "../../../../actions/chatroom_actions";
+import { updateChatroomUser } from "../../../../actions/chatroom_user_actions";
+import { receiveMessage } from "../../../../actions/message_actions";
 
-const mSTP = (state) => {
+const mSTP = state => {
   let chatrooms = selectChatrooms(state);
   let userChatroomIds = selectSubscribedUserChatroomIds(state);
   let chatroomIds = selectChatroomIds(state);
@@ -14,16 +21,23 @@ const mSTP = (state) => {
     chatrooms,
     chatroomIds,
     userChatroomIds,
-    currentUser: state.session.currentUser,
+    currentUser: state.session.currentUser
   };
 };
 
-const mDTP = (dispatch) => ({
+const mDTP = dispatch => ({
   fetchChatrooms: () => dispatch(fetchChatrooms()),
-  updateChatroomUser: (chatroomUser) => dispatch(updateChatroomUser(chatroomUser)),
-  receiveMessage: (message) => dispatch(receiveMessage(message)),
+  updateChatroomUser: chatroomUser =>
+    dispatch(updateChatroomUser(chatroomUser)),
+  receiveMessage: message => dispatch(receiveMessage(message)),
+  fetchChatroom: id => dispatch(fetchChatroom(id))
 });
 
-const SidebarContainer = withRouter(connect(mSTP, mDTP)(Sidebar));
+const SidebarContainer = withRouter(
+  connect(
+    mSTP,
+    mDTP
+  )(Sidebar)
+);
 
 export default SidebarContainer;
