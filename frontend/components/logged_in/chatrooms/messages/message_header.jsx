24a3d12b4f342872949dc9e3683c9d30ecf9ed1b
DrewE <andrew.engelstein@gmail.com>
2019-04-10 11:23:22 -0700

    Add icons and search bar to message header section

--- frontend/components/logged_in/chatrooms/messages/message_header.jsx
+++ frontend/components/logged_in/chatrooms/messages/message_header.jsx
@@ -3,9 +3,22 @@ import React from "react";
 const MessageHeader = props => (
   <>
     <header>
-      <p>
-        # {props.getDMName(props.activeChatroom.name, props.currentUserName)}
-      </p>
+      <div className="mh-left">
+        <p>
+          #{props.getDMName(props.activeChatroom.name, props.currentUserName)}
+        </p>
+      </div>
+      <div className="mh-right">
+        <div className="mh-search">
+          <i className="fas fa-search" />
+          <input type="text" disabled placeholder="Search (coming soon)" />
+        </div>
+        <div className="mh-icons">
+          <i className="fas fa-at" />
+          <i className="far fa-star" />
+          <i className="fas fa-ellipsis-v" />
+        </div>
+      </div>
     </header>
   </>
 );
